4f473a8b8ebf1afc4e32c4c5530280f0f3bdeb4a
Rosando <desouza.rosando@gmail.com>
2017-07-01 00:23:04 +0530

    * Added foods api controller

---
+++ CountingKs/CountingKs/Controllers/FoodsController.cs
@@ -0,0 +1,25 @@
+﻿using CountingKs.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CountingKs.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        public object Get()
+        {
+            var repository = new CountingKsRepository(new CountingKsContext());
+
+            var result = repository.GetAllFoods()
+                                    .OrderBy(x => x.Description)
+                                    .Take(25)
+                                    .ToList();
+
+            return result;
+        }
+    }
+}
