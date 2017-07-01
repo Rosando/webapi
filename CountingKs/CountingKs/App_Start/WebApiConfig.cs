1f1899a0d31bcf9b899c9523f0a1838355c6762d
Rosando <desouza.rosando@gmail.com>
2017-07-01 14:41:33 +0530

    * Added the measures controller and implemented the get method

--- CountingKs/CountingKs/App_Start/WebApiConfig.cs
+++ CountingKs/CountingKs/App_Start/WebApiConfig.cs
@@ -13,10 +13,22 @@ namespace CountingKs
         public static void Register(HttpConfiguration config)
         {
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                name: "Food",
+                routeTemplate: "api/nutrition/foods/{foodid}",
+                defaults: new { controller = "Foods", foodid = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "FoodMeasures",
+                routeTemplate: "api/nutrition/foods/{foodid}/measures/{measureid}",
+                defaults: new { controller = "Measures", measureid = RouteParameter.Optional }
+            );
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
 
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
--- CountingKs/CountingKs/Controllers/MeasuresController.cs
+++ CountingKs/CountingKs/Controllers/MeasuresController.cs
@@ -9,30 +9,36 @@ using System.Web.Http;
 
 namespace CountingKs.Controllers
 {
-    public class FoodsController : ApiController
+    public class MeasuresController : ApiController
     {
         private ICountingKsRepository _countingKsRepository;
         private ModelFactory _modelFactory;
-        public FoodsController(ICountingKsRepository countingKsRepository)
+
+        public MeasuresController(ICountingKsRepository countingKsRepository)
         {
             _countingKsRepository = countingKsRepository;
             _modelFactory = new ModelFactory();
         }
 
-        public IEnumerable<FoodModel> Get()
+        public IEnumerable<MeasureModel> Get(int foodId)
         {
-            var result = _countingKsRepository.GetAllFoodsWithMeasures()
-                                                .OrderBy(x => x.Description)
-                                                .Take(25)
+            var result = _countingKsRepository.GetMeasuresForFood(foodId)
                                                 .ToList()
                                                 .Select(x => _modelFactory.Create(x));
 
             return result;
         }
 
-        public FoodModel Get(int foodid)
+        public MeasureModel Get(int foodId, int measureId)
         {
-            return _modelFactory.Create(_countingKsRepository.GetFood(foodid));
+            var measure = _countingKsRepository.GetMeasure(measureId);
+
+            if(measure.Food.Id != foodId)
+            {
+                return null;
+            }
+
+            return _modelFactory.Create(measure);
         }
     }
 }
